e84d5b9df7e88ca312d965ae66c582ecad02ba2c
Tatsu <tatsuhiroabe0127@gmail.com>
2020-11-07 15:38:08 +0900

    connectDB

--- src/book_log.php
+++ src/book_log.php
@@ -1,46 +1,108 @@
 <?php
 
-function createReviews() {
+function validate($review) {
+
+     $errors = [];
+
+     if (!strlen($review['title'])) {
+          $errors['title'] = '書籍名を入力してください';
+     } elseif (strlen($review['title']) > 255) {
+          $errors['title'] = '書籍名は255文字以内で入力してください';
+     }
+
+     if (!strlen($review['author'])) {
+          $errors['author'] = '著者名を入力してください';
+     } elseif (strlen($review['author']) > 100) {
+          $errors['author'] = '著者名は255文字以内で入力してください';
+     }
+
+     if (!in_array($review['status'], ['未読', '読んでいる', '読了'], true)) {
+          $errors['status'] = '読者状況は、未読、読んでいる、読了の中から選択してください';
+     }
+
+
+     if (1 > $review['score'] || 5 < $review['score']) {
+          $errors['score'] = '1以上5以下の整数で入力してください';
+     }
+
+     if (!strlen($review['summary'])) {
+          $errors['summary'] = '感想を入力してください';
+     } elseif (strlen($review['summary']) > 100) {
+          $errors['summary'] = '感想は1000文字以内で入力してください';
+     }
+
+     return $errors;
+}
+
+function createReviews($link) {
+
+     $reviews = [];
 
      echo '読書ログを登録してください' . PHP_EOL;
      echo '書籍名 ：';
-     $title = trim(fgets(STDIN));
+     $reviews['title'] = trim(fgets(STDIN));
 
      echo '著者名 ：';
-     $author = trim(fgets(STDIN));
+     $reviews['author'] = trim(fgets(STDIN));
 
      echo '読書状況(未読、読んでいる、読了) ：';
-     $status = trim(fgets(STDIN));
+     $reviews['status'] = trim(fgets(STDIN));
 
      echo '評価（5点満点の整数）：';
-     $score = trim(fgets(STDIN));
+     $reviews['score'] = (int) trim(fgets(STDIN));
 
-     echo '感想　：';
-     $summary = trim(fgets(STDIN));
+     echo '感想 ：';
+     $reviews['summary'] = trim(fgets(STDIN));
 
-     echo '登録が完了しました' . PHP_EOL. PHP_EOL;
+     $validated = validate($reviews);
+     if ($validated) {
+          foreach($validated as $error) {
+               echo $error . PHP_EOL;
+          }
+          return;
+     }
 
-     return [
-          'title' => $title,
-          'author' => $author,
-          'status' => $status,
-          'score' => $score,
-          'summary' => $summary,
-     ];
+     $sql =<<<EOD
+     INSERT INTO reviews (
+     title,
+     author,
+     status,
+     score,
+     summary
+     ) VALUES (
+     "{$reviews['title']}",
+     "{$reviews['author']}",
+     "{$reviews['status']}",
+     "{$reviews['score']}",
+     "{$reviews['summary']}"
+     )
+     EOD;
+
+     $result = mysqli_query($link, $sql);
+     if ($result) {
+          echo '登録が完了しました' . PHP_EOL . PHP_EOL;
+     } else {
+          echo 'Error: データの追加に失敗しました' . PHP_EOL;
+          echo 'Debugging Error: ' . mysqli_error($link) . PHP_EOL . PHP_EOL;
+     }
+
 }
 
-function showReviews($reviews) {
+function showReviews($link) {
 
-     echo '書籍名を表示します' . PHP_EOL;
-          foreach($reviews as $review)
-          {
-               echo '書籍名　：' . $review['title'] . PHP_EOL;
-               echo '著者名　：' . $review['author'] . PHP_EOL;
-               echo '読書状況(未読、読んでいる、読了)　：' . $review['status'] . PHP_EOL;
-               echo '評価（5点満点の整数）：' . $review['score'] . PHP_EOL;
-               echo '感想　：' . $review['summary'] . PHP_EOL;
-               echo '-------------' . PHP_EOL;
-          }
+     $sql = 'SELECT title, author, status, score, summary FROM reviews';
+     $result = mysqli_query($link, $sql);
+
+     while ($review = mysqli_fetch_assoc($result)) {
+          echo '書籍名 ：' . $review['title'] . PHP_EOL;
+          echo '著者名 ：' . $review['author'] . PHP_EOL;
+          echo '読書状況(未読、読んでいる、読了) ：' . $review['status'] . PHP_EOL;
+          echo '評価（5点満点の整数）：' . $review['score'] . PHP_EOL;
+          echo '感想 ：' . $review['summary'] . PHP_EOL;
+          echo '-------------' . PHP_EOL;
+     }
+
+     mysqli_free_result($result);
 
 }
 
@@ -54,13 +116,8 @@ function dbConnect()
           exit;
      }
 
-     echo 'データベースに接続できました' . PHP_EOL;
-
      return $link;
 }
-
-$reviews = [];
-
 
 $link = dbConnect();
 
@@ -72,9 +129,9 @@ while (true) {
      $num = trim(fgets(STDIN));
 
      if ($num === '1') {
-          $reviews[] = createReviews();
+          createReviews($link);
      } elseif ($num === '2') {
-          showReviews($reviews);
+          showReviews($link);
      } elseif ($num === '9') {
           mysqli_close($link);
      break;
--- src/tmp/mysqli.php
+++ src/tmp/mysqli.php
@@ -10,19 +10,29 @@ if (!$link) {
 
 echo 'データベースに接続できました' . PHP_EOL;
 
-$sql = <<<EOT
-INSERT INTO companies (
-     name,
-     establishment_date,
-     founder
-) VALUES (
-     'SmartHR Inc',
-     '2013-01-27',
-     'Shoji Miyata'
-)
-EOT;
+// $sql = <<<EOT
+// INSERT INTO companies (
+//      name,
+//      establishment_date,
+//      founder
+// ) VALUES (
+//      'SmartHR Inc',
+//      '2013-01-27',
+//      'Shoji Miyata'
+// )
+// EOT;
 
-mysqli_query($link, $sql);
+// mysqli_query($link, $sql);
+
+$sql = 'SELECT name, founder FROM companies';
+$result = mysqli_query($link, $sql);
+
+while ($company = mysqli_fetch_assoc($result)) {
+     echo '会社名：' . $company['name'] . PHP_EOL;
+     echo '社長名：' . $company['founder'] . PHP_EOL;
+}
+
+mysqli_free_result($result);
 
 mysqli_close($link);
 echo 'データベースとの接続を切断しました' . PHP_EOL;
